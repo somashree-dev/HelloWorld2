60971d44335848a8ba6a21006372660f67e7597f
somashree-dev <59563617+somashree-dev@users.noreply.github.com>
2025-01-27 21:52:18 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,14 @@
+package com.soma;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=5;
+		int num2=8;
+		int result=num1+num2;
+		System.out.println(result);
+
+	}
+
+}
