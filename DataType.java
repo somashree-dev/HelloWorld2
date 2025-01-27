8c4e3ff0e517f1931acace8de07adf7700b90e0c
somashree-dev <59563617+somashree-dev@users.noreply.github.com>
2025-01-27 22:48:20 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,25 @@
+package com.soma;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		
+			int num1=9;
+			System.out.println(num1);
+			byte by=127;
+			System.out.println(by);
+			short sh=558;
+			System.out.println(sh);
+			long l=5854l;
+			System.out.println(l);
+			
+			float f=5.8f;
+			System.out.println(f);
+			double d=5.8;
+			System.out.println(d);			
+			char c='k';
+			System.out.println(c);
+
+	}
+
+}
